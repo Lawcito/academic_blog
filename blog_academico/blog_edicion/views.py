4eacc50c1f829a9cfc35f1855a320c1ad68f5032
Lawcito <121973056+Lawcito@users.noreply.github.com>
2025-12-11 22:41:05 -0300

    add new functionals (search bar, grid/col filter)

--- blog_academico/blog_edicion/views.py
+++ blog_academico/blog_edicion/views.py
@@ -3,19 +3,22 @@ from .models import Articulo, Categoria
 from .forms import ArticuloForm
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseForbidden
+from django.db.models import Q
+
 
 @login_required
 def crear_articulo(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = ArticuloForm(request.POST, request.FILES)
         if form.is_valid():
             articulo = form.save(commit=False)
             articulo.autor = request.user
             articulo.save()
-            return redirect('blog_edicion:articulo_detalle', pk=articulo.pk)
+            return redirect("blog_edicion:articulo_detalle", pk=articulo.pk)
     else:
         form = ArticuloForm()
-    return render(request, 'blog_edicion/crear_articulo.html', {'form': form})
+    return render(request, "blog_edicion/crear_articulo.html", {"form": form})
+
 
 @login_required
 def editar_articulo(request, pk):
@@ -23,25 +26,48 @@ def editar_articulo(request, pk):
     if articulo.autor != request.user:
         return HttpResponseForbidden("No tienes permiso para editar este artículo.")
 
-    if request.method == 'POST':
+    if request.method == "POST":
         form = ArticuloForm(request.POST, request.FILES, instance=articulo)
         if form.is_valid():
             form.save()
-            return redirect('blog_edicion:articulo_detalle', pk=articulo.pk)
+            return redirect("blog_edicion:articulo_detalle", pk=articulo.pk)
     else:
         form = ArticuloForm(instance=articulo)
-    return render(request, 'blog_edicion/editar_articulo.html', {'form': form, 'articulo': articulo})
+    return render(
+        request,
+        "blog_edicion/editar_articulo.html",
+        {"form": form, "articulo": articulo},
+    )
+
 
 def home(request):
-    articulos = Articulo.objects.all()
+    articulos = Articulo.objects.all().order_by("-fecha_publicacion")
     categorias = Categoria.objects.all()
-    return render(request, "index.html", {'articulos': articulos, 'categorias': categorias})
+
+    search_query = request.GET.get("q")
+    if search_query:
+        articulos = articulos.filter(
+            Q(titulo__icontains=search_query) | Q(contenido__icontains=search_query)
+        )
+
+    category_id = request.GET.get("categoria")
+    if category_id:
+        articulos = articulos.filter(categoria__id=category_id)
+    return render(
+        request, "index.html", {"articulos": articulos, "categorias": categorias}
+    )
+
 
 def articulo_detalle(request, pk):
     articulo = get_object_or_404(Articulo, pk=pk)
-    return render(request, 'articulo_detalle.html', {'articulo': articulo})
+    return render(request, "articulo_detalle.html", {"articulo": articulo})
+
 
 def articulos_por_categoria(request, pk):
     categoria = get_object_or_404(Categoria, pk=pk)
     articulos = Articulo.objects.filter(categoria=categoria)
-    return render(request, 'articulos_por_categoria.html', {'categoria': categoria, 'articulos': articulos})
+    return render(
+        request,
+        "articulos_por_categoria.html",
+        {"categoria": categoria, "articulos": articulos},
+    )
