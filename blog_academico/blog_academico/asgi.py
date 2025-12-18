a8b8a9024c7d729c24bbd9fb83fc54f5d548cc14
NekoBamBam <coriannicolas21@gmail.com>
2025-12-18 14:22:39 -0300

    prueba2

--- blog_academico/blog_academico/asgi.py
+++ blog_academico/blog_academico/asgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog_academico.settings')
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE',
+    'blog_academico.blog_academico.settings'
+)
 
 application = get_asgi_application()
+
